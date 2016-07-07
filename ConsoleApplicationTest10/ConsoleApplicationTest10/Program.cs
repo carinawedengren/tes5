3091d98bdde2cd9335ce702ed1fd557d68c97bc5
weden88 <carina.wedengren@gmail.com>
2016-07-07 10:24:00 +0200

    commit

---
+++ ConsoleApplicationTest10/ConsoleApplicationTest10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTest10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hejhej
+            Console.WriteLine();
+            Console.WriteLine("Test");
+        }
+    }
+}
